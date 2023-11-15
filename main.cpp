bb72511320aa80ef7f53912d82e2bda868e863db
sjhryougi <jhoseo00@gmail.com>
2023-11-15 17:46:14 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -36,9 +36,9 @@ int main()
 	}
 
 	Mat result0 = stitch_two_image(frames[11], frames[12]);
-	//Mat result1 = stitch_two_image(result0, frames[13]);
+	Mat result1 = stitch_two_image(result0, frames[13]);
 
-	//stitching을 수행할 때 가운데 이미지를 기준으로 오른쪽으로 스티칭하고 flip을 통해 왼쪽으로 스티칭 후 합친다.
+	//stitching을 수행할 때 가운데 이미지를 기준으로 오른쪽으로 스티칭하고 flip을 통해 왼쪽으로 스티칭 후  합친다.
 	/*int midIndex = frames.size() / 2;
 	Mat result0 = stitch_two_image(frames[midIndex], frames[midIndex + 1]);
 	flip(result0, result0, 1);
@@ -65,7 +65,7 @@ int main()
 
 	
 	imshow("result0", result0);
-	//imshow("result1", result1);
+	imshow("result1", result1);
 	waitKey(0);
 
 	return 0;
@@ -94,37 +94,37 @@ Mat stitch_two_image(Mat original_image, Mat object_image) {
 	bf.match(descriptors1, descriptors2, matches);
 
 	
-	std::sort(matches.begin(), matches.end());
+	/*std::sort(matches.begin(), matches.end());
 
 	int vsize = 0;
 	if (matches.size() >= 50)
 		vsize = 50;
 	else
-		vsize = matches.size();
+		vsize = matches.size();*/
 
 	// 좋은 매칭 선택
-	std::vector<cv::DMatch> good_matches(matches.begin(), matches.begin() + vsize);
+	//std::vector<cv::DMatch> good_matches(matches.begin(), matches.begin() + vsize);
 	
 		
-	//// 좋은 매칭 선택
-	//std::vector<cv::DMatch> good_matches;
-	//double min_dist = 50;
-	//double max_dist = 0;
+	// 좋은 매칭 선택
+	std::vector<cv::DMatch> good_matches;
+	double min_dist = 50;
+	double max_dist = 0;
 
-	//
-	////min-max 
-	//for (int i = 0; i < descriptors1.rows; i++) {
-	//	double dist = matches[i].distance;
-	//	if (dist < min_dist) min_dist = dist;
-	//	if (dist > max_dist) max_dist = dist;
-	//}
+	
+	//min-max 
+	for (int i = 0; i < descriptors1.rows; i++) {
+		double dist = matches[i].distance;
+		if (dist < min_dist) min_dist = dist;
+		if (dist > max_dist) max_dist = dist;
+	}
 
-	//// good match 실행
-	//for (int i = 0; i < descriptors1.rows; i++) {
-	//	if (matches[i].distance < 2 * min_dist) {
-	//		good_matches.push_back(matches[i]);
-	//	}
-	//}
+	// good match 실행
+	for (int i = 0; i < descriptors1.rows; i++) {
+		if (matches[i].distance < 2 * min_dist) {
+			good_matches.push_back(matches[i]);
+		}
+	}
 	
 	
 
@@ -183,24 +183,75 @@ Mat stitch_two_image(Mat original_image, Mat object_image) {
 	imshow("object_on_original1", object_on_original);
 	
 	//이미지의 오른쪽 부분에서 튀어나온 부분을 검출하기 위한 반복문
-	int minCol = object_on_original.cols;
+	//두 좌표의 값을 위한 4개의 변수
+	int minCol = object_on_original.cols - 1;
 	int minRow = 0;
 	int maxCol = 0;
 	int maxRow = 0;
+
+	//오른쪽 부분의 gradient를 구하기 위한 변수
+	bool isPlusGradient;
+
+	//gradient를 구하기 위한 과정
+	//현재는 반복문을 2번 사용해야 하지만 추후에 여유가 되면 개선할 예정
+	for (int i = object_on_original.rows - 1; i >= 0; i--) {
+		for (int j = object_on_original.cols - 1; j >= 0; j--) {
+			if (object_on_original.at<cv::Vec3b>(i, j) != cv::Vec3b(0, 0, 0)) {			
+				//minCol이나 maxCol이 갱신되었을 때 minRow와 maxRow 초기화
+				//minCol과 j가 같을수 있고 이때 값 갱신을 막는다. 때문에 minMax 사용을 포기함
+				if (minCol > j) {
+					minCol = j;
+					minRow = i;
+				}
+
+				if (maxCol < j) {
+					maxCol = j;
+					maxRow = i;
+				}
+				break;
+			}
+		}
+	}
+
+	//두 좌표를 비교해서 gradient를 구한다.
+	//두 좌표가 같을때는 Row는 구별하고 Col은 똑같다 -> 따로 분류할 필요 없음
+	if (minRow >= maxRow)
+		isPlusGradient = true;
+	else
+		isPlusGradient = false;
+
+
+	//gradient를 기반으로 좌표 설정
+	//isPlusGradient가 true인 경우에는 max는 계속 갱신하고 min은 가장 처음 좌표를 유지
+	//false인 경우에는 반대로
 	for (int i = object_on_original.rows - 1; i >= 0; i--) {
 		for (int j = object_on_original.cols - 1; j >= 0; j--) {
 			if (object_on_original.at<cv::Vec3b>(i, j) != cv::Vec3b(0, 0, 0)) {
-				minCol = min(minCol, j);
-				maxCol = max(maxCol, j);
- 
 				//minCol이나 maxCol이 갱신되었을 때 minRow와 maxRow 초기화
-				if (minCol == j)
-					minRow = i;
-				if (maxCol == j)
-					maxRow = i;
+				if (isPlusGradient == true) {
+					if (minCol > j) {
+						minCol = j;
+						minRow = i;
+					}
+
+					if (maxCol <= j) {
+						maxCol = j;
+						maxRow = i;
+					}
+				}
+				else {
+					if (minCol >= j) {
+						minCol = j;
+						minRow = i;
+					}
+
+					if (maxCol < j) {
+						maxCol = j;
+						maxRow = i;
+					}
+				}				
 				break;
 			}
-
 		}
 	}
 
@@ -221,10 +272,11 @@ Mat stitch_two_image(Mat original_image, Mat object_image) {
 		inputPts.push_back(Point2f(minCol, minRow));
 	}
 
-	circle(object_on_original, Point2f(topCol, 100), 3, Scalar(0, 0, 255), 3);
+	//원 생성
+	/*circle(object_on_original, Point2f(topCol, 100), 3, Scalar(0, 0, 255), 3);
 	circle(object_on_original, Point2f(minCol, minRow), 3, Scalar(0, 255, 0), 3);
 	circle(object_on_original, Point2f(bottomCol, originalCutImage.rows - 1 + 100), 3, Scalar(255, 0, 0), 3);
-	circle(object_on_original, Point2f(maxCol, maxRow), 3, Scalar(255, 255, 255), 3);
+	circle(object_on_original, Point2f(maxCol, maxRow), 3, Scalar(255, 255, 255), 3);*/
 	
 
 	outputPts.push_back(Point2f(topCol, 100));
@@ -233,26 +285,49 @@ Mat stitch_two_image(Mat original_image, Mat object_image) {
 	outputPts.push_back(Point2f(minCol, originalCutImage.rows - 1 + 100));
 	imshow("object_on_original2", object_on_original);
 
+
 	//변환행렬 생성
 	Mat M = getPerspectiveTransform(inputPts, outputPts);
 	Mat anotherM;
 	warpPerspective(object_on_original, anotherM, M, object_on_original.size(), INTER_CUBIC);
 	imshow("M", anotherM);
-	cout << endl;
-	cout << "M : " << M << endl;
+
+	//object_on_original과 M을 결합한 Mat 생성 및 값 삽입
+	//빨간점과 파란점을 이은 직선을 경계로 두 이미지를 합친다.
+	float line = (float)(topCol - bottomCol) / (float)originalCutImage.rows;
+	Mat combineImg(originalCutImage.rows, minCol, CV_8UC3);
+	for (int i = 0; i < originalCutImage.rows; i++) {
+		for (int j = 0; j < minCol; j++) {
+			//topCol이 bottomCol보다 큰 경우 빼주고 반대인 경우에는 더한다.
+			if (topCol >= bottomCol) {
+				if (i < topCol - (int)(line * i))
+					combineImg.at<Vec3b>(i,j) = object_on_original.at<Vec3b>(i + 100, j);
+				else
+					combineImg.at<Vec3b>(i, j) = anotherM.at<Vec3b>(i + 100, j);
+			}
+			else {
+				if (i < topCol + (int)(line * i))
+					combineImg.at<Vec3b>(i, j) = object_on_original.at<Vec3b>(i + 100, j);
+				else
+					combineImg.at<Vec3b>(i, j) = anotherM.at<Vec3b>(i + 100, j);
+			}		
+		}
+	}
+
+	imshow("combine", combineImg);
 
 	//결과를 저장할 mat 생성 후 데이터 옮기기
 	//잘린 original과 object_on_original를 합쳐야 한다.
 	//분기문을 넣은 이유는 original_image.cols - originalCutImage.cols가 0이면 오류가 발생하기 때문이다.
-	Mat result = Mat::zeros(object_on_original.rows, original_image.cols - originalCutImage.cols + minCol,CV_8UC3);
+	Mat result = Mat::zeros(combineImg.rows, original_image.cols - originalCutImage.cols + combineImg.cols,CV_8UC3);
 	if (original_image.cols - originalCutImage.cols == 0) {
-		object_on_original(Rect(0, 0, minCol, originalCutImage.rows)).
+		combineImg(Rect(0, 0, minCol, originalCutImage.rows)).
 			copyTo(result(Rect(original_image.cols - originalCutImage.cols, 0, minCol, originalCutImage.rows)));
 	}
 	else {
 		original_image(Rect(0, 0, original_image.cols - originalCutImage.cols, originalCutImage.rows)).
 			copyTo(result(Rect(0, 0, original_image.cols - originalCutImage.cols, originalCutImage.rows)));
-		object_on_original(Rect(0, 0, minCol, originalCutImage.rows)).
+		combineImg(Rect(0, 0, minCol, originalCutImage.rows)).
 			copyTo(result(Rect(original_image.cols - originalCutImage.cols, 0, minCol, originalCutImage.rows)));
 	}
 	waitKey(1);
